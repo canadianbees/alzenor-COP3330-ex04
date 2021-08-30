207caa14817dadfbce6a332631d9f9727f17a6c3
Celina Alzenor <you@example.com>
2021-08-30 14:28:30 -0400

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+/*
+ *  UCF COP3330 Fall 2021 Exercise 4 Solution
+ *  Copyright 2021 first_name last_name
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        madLib story = new madLib();
+
+        story.storyTime();
+    }
+}
---
+++ src/main/java/org/example/madLib.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class madLib
+{
+    String noun;
+    String verb;
+    String adjective;
+    String adverb;
+    String friend;
+
+    public madLib()
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a noun: ");
+        this.noun = scan.nextLine();
+
+        System.out.print("Enter a verb: ");
+        this.verb = scan.nextLine();
+
+        System.out.print("Enter an adjective: ");
+        this.adjective = scan.nextLine();
+
+        System.out.print("Enter an adverb: ");
+        this.adverb = scan.nextLine();
+
+        System.out.print("Enter a name for your friend: ");
+        this.friend = scan.nextLine();
+    }
+
+    public void storyTime()
+    {
+        System.out.println("You and "+this.friend+" are in the park and you see a mysterious figure in the lake. You both go towards it and it grabs "
+                +this.friend+" and pulls them into the murky lake water. \nIn a panic, you dive into the water to go after "+this.friend+". " +
+                "\n\nAll of a sudden, the mysterious figure appears and when you look closer it turns out to be a "+this.noun+"!. " +
+                "The "+this.noun+" looks at you and you jump when you notice how "+this.adjective+" it is. \nYou demand that it hands over "+this.friend+" but" +
+                " unfortunately it already ate them! Quickly, you "+this.adverb+" swim up to the surface and never went back to the park again!");
+    }
+}
