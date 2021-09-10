511a27496aea96428216ef551f48c09527dd114f
canadianbees <88695497+canadianbees@users.noreply.github.com>
2021-09-09 22:50:59 -0400

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,17 @@
-package org.example;
 /*
  *  UCF COP3330 Fall 2021 Exercise 4 Solution
- *  Copyright 2021 first_name last_name
+ *  Copyright 2021 Celina Alzenor
  */
+package org.example;
+
 public class Main
 {
     public static void main( String[] args )
     {
+        //user input for noun,adjective,adverb, etc.
         madLib story = new madLib();
 
+        //constructs madLib story
         story.storyTime();
     }
 }
